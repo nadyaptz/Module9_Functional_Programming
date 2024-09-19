ab5ba2dee52a6a1bdf109710e388efebf246006c
Надя <nadya.ptz@gmail.com>
2024-09-19 07:14:53 +0300

    initial

---
+++ Module_9_3.py
@@ -0,0 +1,10 @@
+first = ['Strings', 'Student', 'Computers']
+second = ['Строка', 'Урбан', 'Компьютер']
+
+first_result = (len(i) - len(j) for i, j in zip(first, second) if len(i) != len(j))
+
+second_result = (len(first[i]) == len(second[i]) for i in range(len(first)) if len(first) == len(second))
+
+#
+print(list(first_result))
+print(list(second_result))
